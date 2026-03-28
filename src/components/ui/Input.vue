ebe3eaba327a7606ab1730b73a50a53b7ee74b81
marouaneMJH <marwanmoujahid008@gmail.com>
2026-03-28 23:07:19 +0100

    feat: implement defineModel for Input and Textarea components to simplify v-model binding

--- src/components/ui/Input.vue
+++ src/components/ui/Input.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { cn } from "@/lib/utils";
+const modelValue = defineModel<string | number>();
 
 interface Props {
     class?: string;
@@ -10,6 +11,7 @@ withDefaults(defineProps<Props>(), {});
 
 <template>
     <input
+        v-model="modelValue"
         :class="
             cn(
                 'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
--- src/components/ui/Textarea.vue
+++ src/components/ui/Textarea.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { cn } from "@/lib/utils";
+const modelValue = defineModel<string>();
 
 interface Props {
     class?: string;
@@ -11,6 +12,7 @@ const props = defineProps<Props>();
 <template>
     <textarea
         v-bind="$attrs"
+        v-model="modelValue"
         :class="
             cn(
                 'flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm text-foreground ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
